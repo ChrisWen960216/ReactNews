d331ea859c9e72145669fced6566cbde48edee61
ChrisWen960216 <957638221@qq.com>
2017-04-13 10:04:29 +0800

    push my change 17.4.13

--- src/js/components/pc_header.js
+++ src/js/components/pc_header.js
@@ -121,35 +121,35 @@ class PCHeader extends React.Component {
 							selectedKeys={[this.state.current]}>
 
 							<Menu.Item key="top">
-								<Icon type="appstore"/>头条
+								<Icon type="appstore" spin={true}/>头条
 							</Menu.Item>
 
 							<Menu.Item key="shehui">
-								<Icon type="appstore"/>社会
+								<Icon type="appstore" spin={true}/>社会
 							</Menu.Item>
 
 							<Menu.Item key="guonei">
-								<Icon type="appstore"/>国内
+								<Icon type="appstore" spin={true}/>国内
 							</Menu.Item>
 
 							<Menu.Item key="guoji">
-								<Icon type="appstore"/>国际
+								<Icon type="appstore" spin={true}/>国际
 							</Menu.Item>
 
 							<Menu.Item key="yule">
-								<Icon type="appstore"/>娱乐
+								<Icon type="appstore" spin={true}/>娱乐
 							</Menu.Item>
 
 							<Menu.Item key="tiyu">
-								<Icon type="appstore"/>体育
+								<Icon type="appstore" spin={true}/>体育
 							</Menu.Item>
 
 							<Menu.Item key="keji">
-								<Icon type="appstore"/>科技
+								<Icon type="appstore" spin={true}/>科技
 							</Menu.Item>
 
 							<Menu.Item key="shishang">
-								<Icon type="appstore"/>时尚
+								<Icon type="appstore" spin={true}/>时尚
 							</Menu.Item>
 							
 							{userShow}
